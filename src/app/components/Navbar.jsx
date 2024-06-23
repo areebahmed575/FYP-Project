ebcd895c51a374206b4302555ed9d38deda2a247
areebahmed575 <areebahmed575@gmail.com>
2024-06-24 00:03:52 +0500

    Minor changes

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center p-4 bg-gray-100 text-gray-500">
-      <div className="text-2xl font-bold">Toursim Website</div>
+    <div className="flex justify-between items-center p-4 bg-gray-100 text-gray-800">
+      <div className="text-2xl font-bold">Gen AI Powered Toursim Website</div>
       <div>
-        <button className=" bg-white text-gray-500  px-4 py-2 rounded">Sign in</button>
+        <button className=" bg-white text-gray-800  px-4 py-2 rounded">Sign in</button>
       </div>
     </div>
   );
