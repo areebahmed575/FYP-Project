996bbae5a843be98b1aaa04065413ea2b0a023c7
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-07-25 19:15:25 +0500

    trending box

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,21 @@
 import SearchBar from "./components/SearchBar/SearchBar"
+import TrendingBox from "./components/TrendingBox/TrendingBox"
 
 const HomePage = () => {
     return (
         <main className="flex flex-col ">
-            <div className="searchContainer w-[60%] m-auto">
+            <div className="searchContainer flex justify-center">
                 <SearchBar />
             </div>
-            <p>Home page under works</p>
+            <h1 className="text-[32px] font-bold mt-[40px]">Tranding Destinations</h1>
+            <p className="text-gray-400">Most popular choices for travellers</p>
+            <div className="trendingContainer flex items-center justify-between flex-wrap my-[30px]">
+                <TrendingBox />
+                <TrendingBox />
+                <TrendingBox />
+                <TrendingBox />
+                <TrendingBox />
+            </div>
         </main>
     )
 }
