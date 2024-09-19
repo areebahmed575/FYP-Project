ad4061dc30fa79d2be87ffe2a74fd00f44a0aaba
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-09-19 11:58:38 +0500

    some changes

--- src/app/SignIn/page.jsx
+++ src/app/SignIn/page.jsx
@@ -40,7 +40,7 @@ const Register = async () => {
                     </div>
                     <motion.button whileHover={{ scale: 1.05 }}
                         whileTap={{ scale: 0.95 }} className="bg-gradient-to-r from-teal-500 to-blue-500 text-white px-4 py-2 rounded-full shadow-md hover:shadow-lg transition duration-300 flex items-center space-x-2">
-                        <span>Sign In</span>
+                        <span className="bg-gradient-to-r from-teal-500 to-blue-500 text-white px-4 py-2 rounded-full shadow-md hover:shadow-lg transition duration-300 flex items-center space-x-2">Sign In</span>
                     </motion.button>
                 </div>
             </div>
--- src/app/components/LoginInput/LoginInputs.jsx
+++ src/app/components/LoginInput/LoginInputs.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-const LoginInputs = () => {
+const  LoginInputs = () => {
     return (
         <div className="inputs flex flex-col gap-[15px]">
             <input type="text" placeholder='Username' className='w-[300px] py-[5px] px-[10px] rounded bg-white border-teal-500 border-[2px] outline-teal-700 placeholder:text-theme-black' />
